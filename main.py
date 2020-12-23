aac864b8992de39fac55a855e12105265262d259
Alexander Spiesberger <alex.spiesberger@gmail.com>
2020-12-23 00:08:47 +0000

    Already input output response

--- main.py
+++ main.py
@@ -15,15 +15,45 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
+
     msg = message.content
 
     if msg.startswith('$play'):
+        channel = message.channel
+        author = message.author
+
+        def random_choice():
+
+
+        def check(m):
+            """
+            Checks if the wait_for is correct
+            """
+            return m.content in possibilities and m.author == author and m.channel == channel
+
+        response = await client.wait_for('message', check=check)
+
+        await channel.send('Your response: {}'.format(response.content))
+
+        '''curr_time = time.time()
+        await message.channel.send(curr_time)
+        # if $play -> 1 at a time
+        time.sleep(3)
+        await message.channel.send('GO')
+
+        if message.content.startswith('$time'):
+            if (time.time() > curr_time +2) and (time.time() < curr_time +4):
+                await message.channel.send('YEE')'''
+
+
+        '''beginning_time = time.time()
         for i in range(5, -1, -1):
+            time.sleep(1)
             if i == 0:
-                await message.channel.send('Go')
+                await message.channel.send('@{0.author} Go'.format(message))
             else:
-                await message.channel.send(i)
+                await message.channel.send(i)'''
         #await message.channel.send('')
         #await message.channel.send(response)
 
+client.run(os.getenv('TOKEN'))
